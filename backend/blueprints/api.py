f5df3e9e4276911680d0cbe9e97b1ba9701f03da
Mike <63655075+MixelTe@users.noreply.github.com>
2023-08-09 00:46:19 +0300

    backend: closing db session

--- backend/blueprints/api.py
+++ backend/blueprints/api.py
@@ -1,9 +1,9 @@
 from flask import Blueprint, jsonify, request
 from flask_jwt_extended import get_jwt_identity, jwt_required
-from data import db_session
+from sqlalchemy.orm import Session
 import logging
+from utils import use_db_session
 from data.event import Event
-
 from data.user import User
 
 
@@ -12,19 +12,16 @@ blueprint = Blueprint("api", __name__)
 
 @blueprint.route("/api/user")
 @jwt_required()
-def user():
+@use_db_session()
+def user(db_sess: Session):
     user_id = get_jwt_identity()
-
-    db_sess = db_session.create_session()
     user: User = db_sess.query(User).filter(User.id == user_id).first()
-
     return jsonify(user.get_dict()), 200
 
 
 @blueprint.route("/api/events")
 @jwt_required()
-def events():
-    db_sess = db_session.create_session()
+@use_db_session()
+def events(db_sess: Session):
     events = db_sess.query(Event).all()
-
     return jsonify(list(map(lambda x: x.get_dict(), events))), 200
--- backend/blueprints/authentication.py
+++ backend/blueprints/authentication.py
@@ -1,16 +1,17 @@
 from flask import Blueprint, jsonify, request
 from flask_jwt_extended import create_access_token, unset_jwt_cookies, set_access_cookies
+from sqlalchemy.orm import Session
 import logging
-from data import db_session
 from data.user import User
-from utils import get_json, get_json_values
+from utils import get_json, get_json_values, use_db_session
 
 
 blueprint = Blueprint("authentication", __name__)
 
 
 @blueprint.route("/api/auth", methods=["POST"])
-def login():
+@use_db_session()
+def login(db_sess: Session):
     data, is_json = get_json(request)
     if not is_json:
         return jsonify({"msg": "body is not json"}), 415
@@ -20,7 +21,6 @@ def login():
     if values_error:
         return jsonify({"msg": values_error}), 400
 
-    db_sess = db_session.create_session()
     user: User = db_sess.query(User).filter(User.login == login).first()
 
     if not user or not user.check_password(password):
