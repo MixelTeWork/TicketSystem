26f54fb8dd8c7d36d30fba9a70feebac27dcbb77
Mike <63655075+MixelTe@users.noreply.github.com>
2023-08-09 22:10:08 +0300

    frontend: centeredPage Layout

--- frontend/src/components/Layout/index.tsx
+++ frontend/src/components/Layout/index.tsx
@@ -2,12 +2,19 @@ import classNames from "../../utils/classNames";
 import Header from "../Header";
 import styles from "./styles.module.css"
 
-export default function Layout({ children, className, centered = false, height100 = false, gap = 0, header = <Header /> }: LayoutProps)
+export default function Layout({ children, className, centered = false, centeredPage = false, height100 = false, gap = 0, header = <Header /> }: LayoutProps)
 {
 	return (
 		<div className={styles.root} style={{ maxHeight: height100 ? "100dvh" : "" }}>
 			{header || <div></div>}
-			<div className={classNames(styles.body, centered && styles.body_centered, className)} style={{ gap, maxHeight: height100 ? "100%" : "" }}>
+			<div
+				className={classNames(
+					styles.body, className,
+					centered && styles.body_centered,
+					centeredPage && styles.body_centeredPage,
+				)}
+				style={{ gap, maxHeight: height100 ? "100%" : "" }}
+			>
 				{children}
 			</div>
 		</div>
@@ -16,6 +23,7 @@ export default function Layout({ children, className, centered = false, height10
 
 interface LayoutProps extends React.PropsWithChildren
 {
+	centeredPage?: boolean,
 	centered?: boolean,
 	height100?: boolean,
 	gap?: number | string,
