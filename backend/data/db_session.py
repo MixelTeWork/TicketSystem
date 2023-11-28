9ffacbff1a6ba6d9c90608fb1ab99b2693848015
Mike <63655075+MixelTe@users.noreply.github.com>
2023-11-28 20:02:15 +0300

    add naming_convention

--- backend/data/db_session.py
+++ backend/data/db_session.py
@@ -3,7 +3,15 @@ import sqlalchemy.orm as orm
 from sqlalchemy.orm import Session
 import sqlalchemy.ext.declarative as dec
 
+convention = {
+    "ix": 'ix_%(column_0_label)s',
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
 SqlAlchemyBase = dec.declarative_base()
+SqlAlchemyBase.metadata = sa.MetaData(naming_convention=convention)
 
 __factory = None
 
