88fb5df52f0b4a3bfbe2fdd3bdbfd139cc6479d5
Mike <63655075+MixelTe@users.noreply.github.com>
2025-03-06 00:17:56 +0300

    improve logging

--- backend/bfs/logger.py
+++ backend/bfs/logger.py
@@ -1,7 +1,9 @@
-import json
-from flask import g, has_request_context, request
 from datetime import datetime, timedelta
+import json
 import logging
+import logging.handlers
+
+from flask import g, has_request_context, request
 
 import bfs_config
 
@@ -65,25 +67,37 @@ def setLogging():
     logging.Formatter.converter = customTime
 
     formatter_error = RequestFormatter("[%(asctime)s] (%(req_id)s by uid=%(uid)-6s) %(method)-6s %(url)-40s | %(levelname)s in %(module)s (%(name)s):\nReq json: %(json)s\n%(message)s\n")  # noqa: E501
-    file_handler_error = logging.FileHandler(bfs_config.log_errors_path, mode="a", encoding="utf-8")
+    file_handler_error = logging.handlers.RotatingFileHandler(
+        bfs_config.log_errors_path, mode="a", encoding="utf-8", maxBytes=4 * 1000 * 1000, backupCount=100)
     file_handler_error.setFormatter(formatter_error)
     file_handler_error.setLevel(logging.WARNING)
     file_handler_error.encoding = "utf-8"
     logger.addHandler(file_handler_error)
 
-    formatter_info = RequestFormatter("%(req_id)s;%(uid)-6s;%(asctime)s;%(method)s;%(url)s;%(levelname)s;%(message)s")
-    formatter_info.max_msg_len = 512
-    file_handler_info = logging.FileHandler(bfs_config.log_path, mode="a", encoding="utf-8")
+    formatter_info = RequestFormatter("%(req_id)s;%(uid)-6s;%(asctime)s;%(method)s;%(url)s;%(levelname)s;%(module)s;%(message)s")
+    file_handler_info = logging.handlers.RotatingFileHandler(
+        bfs_config.log_path, mode="a", encoding="utf-8", maxBytes=4 * 1000 * 1000, backupCount=100)
     file_handler_info.setFormatter(formatter_info)
     file_handler_info.addFilter(InfoFilter())
     file_handler_info.encoding = "utf-8"
     logger.addHandler(file_handler_info)
 
+    logger_requests = get_logger_requests()
+    formatter_req = RequestFormatter("%(req_id)s;%(uid)-6s;%(asctime)s;%(method)s;%(url)s;%(levelname)s;%(message)s")
+    formatter_req.max_msg_len = 512
+    file_handler_req = logging.handlers.RotatingFileHandler(
+        bfs_config.log_requests_path, mode="a", encoding="utf-8", maxBytes=4 * 1000 * 1000, backupCount=100)
+    file_handler_req.setFormatter(formatter_req)
+    file_handler_req.setLevel(logging.INFO)
+    file_handler_req.encoding = "utf-8"
+    logger_requests.addHandler(file_handler_req)
+
     logger_frontend = get_logger_frontend()
     formatter_frontend = RequestFormatter("[%(asctime)s] (uid=%(uid)s):\n%(json)s\n%(message)s\n")
     formatter_frontend.max_json_len = -1
     formatter_frontend.json_indent = 4
-    file_handler_frontend = logging.FileHandler(bfs_config.log_frontend_path, mode="a", encoding="utf-8")
+    file_handler_frontend = logging.handlers.RotatingFileHandler(
+        bfs_config.log_frontend_path, mode="a", encoding="utf-8", maxBytes=4 * 1000 * 1000, backupCount=100)
     file_handler_frontend.setFormatter(formatter_frontend)
     file_handler_frontend.setLevel(logging.INFO)
     file_handler_frontend.encoding = "utf-8"
@@ -92,6 +106,10 @@ def setLogging():
 
 def get_logger_frontend():
     return logging.getLogger("frontend")
+
+
+def get_logger_requests():
+    return logging.getLogger("requests")
 
 
 def log_frontend_error():
