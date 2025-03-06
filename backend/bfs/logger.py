73c785f1434d61e9b858d6d94824668d77215276
Mike <63655075+MixelTe@users.noreply.github.com>
2025-03-06 20:55:06 +0300

    fix logging

--- backend/bfs/logger.py
+++ backend/bfs/logger.py
@@ -2,6 +2,7 @@ from datetime import datetime, timedelta
 import json
 import logging
 import logging.handlers
+import os
 
 from flask import g, has_request_context, request
 
@@ -55,6 +56,42 @@ class RequestFormatter(logging.Formatter):
         return super().format(record)
 
 
+class RotatingFileHandler(logging.handlers.RotatingFileHandler):
+    def __init__(self, *kargs, **kwargs):
+        self.rollover = False
+        super().__init__(*kargs, **kwargs)
+
+    def doRollover(self):
+        self.rollover = True
+        s = self.stream
+        self.stream = self._open()
+        if s:
+            s.close()
+
+    def _open(self):
+        filename = self.baseFilename
+        i = 0
+        n = self.baseFilename.split(".")
+        name, ext = (".".join(n[:-1]), n[-1]) if len(n) > 1 else (n[0], "")
+        while True:
+            i += 1
+            filename = self.rotation_filename("%s.%d.%s" % (name, i, ext))
+            if not os.path.exists(filename):
+                if self.rollover:
+                    self.rollover = False
+                else:
+                    if i - 1 == 0:
+                        filename = self.baseFilename
+                    else:
+                        filename = self.rotation_filename("%s.%d.%s" % (name, i - 1, ext))
+                break
+        bn = self.baseFilename
+        self.baseFilename = filename
+        r = super()._open()
+        self.baseFilename = bn
+        return r
+
+
 def setLogging():
     logging.basicConfig(
         level=logging.DEBUG,
@@ -67,16 +104,16 @@ def setLogging():
     logging.Formatter.converter = customTime
 
     formatter_error = RequestFormatter("[%(asctime)s] (%(req_id)s by uid=%(uid)-6s) %(method)-6s %(url)-40s | %(levelname)s in %(module)s (%(name)s):\nReq json: %(json)s\n%(message)s\n")  # noqa: E501
-    file_handler_error = logging.handlers.RotatingFileHandler(
-        bfs_config.log_errors_path, mode="a", encoding="utf-8", maxBytes=4 * 1000 * 1000, backupCount=100)
+    file_handler_error = RotatingFileHandler(
+        bfs_config.log_errors_path, mode="a", encoding="utf-8", maxBytes=4 * 1000 * 1000)
     file_handler_error.setFormatter(formatter_error)
     file_handler_error.setLevel(logging.WARNING)
     file_handler_error.encoding = "utf-8"
     logger.addHandler(file_handler_error)
 
     formatter_info = RequestFormatter("%(req_id)s;%(uid)-6s;%(asctime)s;%(method)s;%(url)s;%(levelname)s;%(module)s;%(message)s")
-    file_handler_info = logging.handlers.RotatingFileHandler(
-        bfs_config.log_path, mode="a", encoding="utf-8", maxBytes=4 * 1000 * 1000, backupCount=100)
+    file_handler_info = RotatingFileHandler(
+        bfs_config.log_path, mode="a", encoding="utf-8", maxBytes=4 * 1000 * 1000)
     file_handler_info.setFormatter(formatter_info)
     file_handler_info.addFilter(InfoFilter())
     file_handler_info.encoding = "utf-8"
@@ -85,8 +122,8 @@ def setLogging():
     logger_requests = get_logger_requests()
     formatter_req = RequestFormatter("%(req_id)s;%(uid)-6s;%(asctime)s;%(method)s;%(url)s;%(levelname)s;%(message)s")
     formatter_req.max_msg_len = 512
-    file_handler_req = logging.handlers.RotatingFileHandler(
-        bfs_config.log_requests_path, mode="a", encoding="utf-8", maxBytes=4 * 1000 * 1000, backupCount=100)
+    file_handler_req = RotatingFileHandler(
+        bfs_config.log_requests_path, mode="a", encoding="utf-8", maxBytes=4 * 1000 * 1000)
     file_handler_req.setFormatter(formatter_req)
     file_handler_req.setLevel(logging.INFO)
     file_handler_req.encoding = "utf-8"
@@ -96,8 +133,8 @@ def setLogging():
     formatter_frontend = RequestFormatter("[%(asctime)s] (uid=%(uid)s):\n%(json)s\n%(message)s\n")
     formatter_frontend.max_json_len = -1
     formatter_frontend.json_indent = 4
-    file_handler_frontend = logging.handlers.RotatingFileHandler(
-        bfs_config.log_frontend_path, mode="a", encoding="utf-8", maxBytes=4 * 1000 * 1000, backupCount=100)
+    file_handler_frontend = RotatingFileHandler(
+        bfs_config.log_frontend_path, mode="a", encoding="utf-8", maxBytes=4 * 1000 * 1000)
     file_handler_frontend.setFormatter(formatter_frontend)
     file_handler_frontend.setLevel(logging.INFO)
     file_handler_frontend.encoding = "utf-8"
