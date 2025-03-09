0b2b9e02c572580fee455210f94cb478185ad3d8
MixelTe <63655075+MixelTe@users.noreply.github.com>
2025-03-09 12:52:08 +0000

    Deploying to gh-pages from @ MixelTeWork/TicketSystem@4c22d8ea49e58ba6edf123f99a037d945e697d6c 🚀

--- bfs/data/log.py
+++ bfs/data/log.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from typing import Any
 
-from sqlalchemy import Column, DateTime, ForeignKey, orm, Integer, String, JSON
+from sqlalchemy import Column, DateTime, orm, Integer, String, JSON
 from sqlalchemy.orm import Session
 
 from .. import SqlAlchemyBase, TableBase, UserBase, IdMixin, get_datetime_now
@@ -16,13 +16,11 @@ class Log(SqlAlchemyBase, IdMixin):
 
     date = Column(DateTime, nullable=False)
     actionCode = Column(String(16), nullable=False)
-    userId = Column(Integer, ForeignKey("User.id"), nullable=False)
+    userId = Column(Integer, nullable=False)
     userName = Column(String(64), nullable=False)
     tableName = Column(String(16), nullable=False)
     recordId = Column(Integer, nullable=False)
     changes = Column(JSON, nullable=False)
-
-    user = orm.relationship("User")
 
     def __repr__(self):
         return f"<Log> [{self.id}] {self.date} {self.actionCode}"
--- bfs/utils/create_file_response.py
+++ bfs/utils/create_file_response.py
@@ -1,7 +1,10 @@
-from flask import current_app, send_file
+import os
+from flask import current_app, make_response, send_file
 
 
 def create_file_response(fpath: str, ftype: str, fname: str):
+    if not os.path.exists(fpath):
+        return make_response("", 404)
     max_age = current_app.config["CACHE_MAX_AGE"]
     response = send_file(fpath)
     response.headers.set("Content-Type", ftype)
